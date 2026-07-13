da1ac51aff13b86f9635c75d8d661bbba0e9f3fb
Fabio Silveira <jmhomeimprovement.uk@gmail.com>
2026-07-13 21:19:37 +0100

    Corrigir alinhamento das features

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log("JM Home Improvement");
+const menuButton = document.querySelector(".menu-toggle");
+const navigationMenu = document.querySelector("nav ul");
+
+menuButton.addEventListener("click", () => {
+    navigationMenu.classList.toggle("active");
+});
+
+navigationMenu.querySelectorAll("a").forEach((link) => {
+    link.addEventListener("click", () => {
+        navigationMenu.classList.remove("active");
+    });
+});
